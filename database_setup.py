b68285104b7a3aee6d925fb0dfc18d40b3f8aa5e
Vanessa Osuka <vanessaosuka@gmail.com>
2018-02-14 12:43:40 +0100

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,5 @@
 import sys
-from sqlalchemy import Column, Integer, String, ForeignKey
+from flask_sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship
